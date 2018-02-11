86939cc25a46f174cc88802ddb8e78a2f60777d6
Robert Amour <robert_amour@elcamino.edu>
2018-02-10 21:11:08 -0800

    Add World class

---
+++ World.cpp
@@ -0,0 +1,5 @@
+//
+// Created by KunstDerFuge on 2/10/18.
+//
+
+#include "World.h"
---
+++ World.h
@@ -0,0 +1,21 @@
+//
+// Created by KunstDerFuge on 2/10/18.
+//
+
+#ifndef HEADSPACE_WORLD_H
+#define HEADSPACE_WORLD_H
+
+
+#include <vector>
+class Creature;
+class Player;
+
+class World {
+private:
+    std::vector<Creature*> creatures;
+    Player* player;
+public:
+};
+
+
+#endif //HEADSPACE_WORLD_H
